dbbd7c5d264e2c0e167376a8e406c397571d28f7
Kousei Yajima <gomadare0818@gmail.com>
2025-12-28 22:47:54 +0900

    change code

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,28 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+	if (!begin || !end || begin > end)
+		return false;
+
+	if (end - begin < 2)
+		return true;
+
+	bool swapped = true;
+	while (swapped)
+	{
+		swapped = false;
+		for (item* p = begin; p + 1 < end; ++p)
+		{
+			if (p->key > (p + 1)->key)
+			{
+				item tmp = *p;
+				*p = *(p + 1);
+				*(p + 1) = tmp;
+				swapped = true;
+			}
+		}
+		--end;
+	}
+
+	return true;
 }
